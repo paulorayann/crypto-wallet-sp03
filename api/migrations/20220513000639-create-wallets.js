78b93d50f059ae409682bac92918dc8f2a7bdbcd
Paulo Rayann <paulorpsena@gmail.com>
2022-05-12 21:41:39 -0300

    feat: creating database models

---
+++ api/migrations/20220513000639-create-wallets.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Wallets', {
+      address: {
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      cpf: {
+        type: Sequelize.STRING
+      },
+      birthDate: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Wallets');
+  }
+};
