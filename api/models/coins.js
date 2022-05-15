fbd84216e8c9e07ab78c4cf65b22ea0fadb1bfa7
Paulo Rayann <paulorpsena@gmail.com>
2022-05-15 16:33:39 -0300

    feat: Coins associations

--- api/models/coins.js
+++ api/models/coins.js
@@ -4,16 +4,18 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Coins extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    // static associate(models) {
-    //   Coins.hasMany(models.Wallets, {
-    //     foreignKey: 'walletAddress'
-    //   })
-    // }
+
+    static associate(models) {
+
+      Coins.hasMany(models.Transactions, {
+        foreignKey: 'coinId'
+      })
+      Coins.belongsTo(models.Wallets, {
+        foreignKey: 'walletId'
+      })
+
+    }
+
   }
   Coins.init({
   id: {
